52f284ed71ffdfeb8d14b2808807ed8819463443
tribalius <tribaliuscz@gmail.com>
2016-04-14 09:56:41 +0200

    Update db.php

--- tridy/db.php
+++ tridy/db.php
@@ -36,7 +36,12 @@ class db {
    }
    
    /**
-    * 
+    * dale je zde moznost vytvorit novy zaznam pomoci odkazu, ktery presmeruje na form.php
+    * @param string v ['Jmeno'] pomoci foreach se vypisuje Jmeno u kazdeho zaznamu
+    * @param string v ['Prijmeni'] pomoci foreach se vypisuje Prijmeni u kazdeho zaznamu
+    * @param string v ['Stat'] pomoci foreach se vypisuje Stat u kazdeho zaznamu
+    * @param string v ['Nazev'] pomoci foreach se vypisuje Umelecke jmeno u kazdeho zaznamu
+    * @return vrací tabulku v html, ktera je naplnena daty z databaze a navic ke kazdemu zaznamu v tabulce pridava moznost smazat tento zaznam ci jej zmenit
     */
    public static function vypis(){
        echo ("<table border='1'><tr><th>Id</th><th>Jméno</th><th>Přijmení</th><th>Stát</th><th>Umělecké jméno / Skupina</th><th><form action=form.php method='POST'><input type='image' src='icons/new.png' style='width: 30px; height: 30px'></form></th></tr>");
@@ -54,10 +59,23 @@ class db {
        echo ('<table>');
    }
    
+   /**
+    * @param int radek oznacuje ktery radek chce uzivatel smazat
+    * @param int id promenna do ktere se ulozi id podle parametru radek
+    * @return smaze urceny radek dle id
+    */
    public static function delete($radek){
        $id = self::dotaz()[$radek]['Id'];
        mysqli_query(self::$pripojeni, "DELETE FROM lide WHERE Id=".$id);
    }
+   
+   /**
+    * @param string jmeno vlozene jmeno do form.php, ktere chce uzivatel pridat do databaze
+    * @param string prijmeni vlozene prijmeni do form.php, ktere chce uzivatel pridat do databaze
+    * @param string stat vlozeny stat do form.php, ktery chce uzivatel pridat do databaze
+    * @param int artist vlozeny artist do form.php, ke kteremu chce uzivatel pripsat cloveka
+    * @return vlozi do databaze jmeno, prijmeni, stat a pokud je zadan tak i artist
+    */
    public static function novy($jmeno, $prijmeni, $stat,$artist){
        if ($artist != ''){
           $dotaz = mysqli_query(self::$pripojeni, "INSERT INTO lide (Jmeno, Prijmeni, Stat, Artist) VALUES ('".$jmeno."', '".$prijmeni."', '".$stat."', '".$artist."')"); 
@@ -67,9 +85,25 @@ class db {
        }
        
    }
+   
+   /**
+    * @param int id oznacuje zaznam v databazi, ktery ma byt aktualizovan
+    * @param string jmeno urcuje na jake jmeno ma byt dany zaznam prepsan
+    * @param string prijmeni urcuje na jake prijmeni ma byt dany zaznam prepsan
+    * @param string stat urcuje na jaký stat ma byt dany zaznam prepsan
+    * @param int artist urcuje k jakemu artistovy ma byt dany zaznam pripsan
+    * @return prepise vsechny bunky u daneho zaznamu podle odeslaneho formulare
+    */
    public static function update($id, $jmeno, $prijmeni, $stat, $artist){
        $dotaz = mysqli_query(self::$pripojeni, "UPDATE lide SET Jmeno ='".$jmeno."', Prijmeni ='".$prijmeni."', Stat ='".$stat."', Artist ='".$artist."' WHERE Id=".$id."");
    }
+   
+   /**
+    * @param int radek urcuje radek ktery ma byt aktualizovan
+    * @param int id urcuje id zaznamu, ktery ma byt aktualizovan
+    * @param array napln v sobe nese data o urcenem zaznamu z databaze, ktery ma byt aktualizovan
+    * @returt data o urcenem zaznamu, ktera naplni vytvoreni formular, aby uzivatel nemusel psat veskera data, ktera nechce menit
+    */
    public static function napln(){
        $radek = filter_input(INPUT_GET, 'radek');
        $id = self::dotaz()[$radek]['Id'];
